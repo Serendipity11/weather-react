ac753e05a4df083418fbd5d49494fb3ecec33f71
V S <66644986+Serendipity11@users.noreply.github.com>
2021-05-05 12:48:37 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
-      <footer>
-        This project is
-        <a href="https://github.com/Serendipity11/weather-react">
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project is
+          <a href="https://github.com/Serendipity11/weather-react">
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
