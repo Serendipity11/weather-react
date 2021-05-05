ba455d3d24224e2b6ed046c3e110a1c80afd1725
V S <66644986+Serendipity11@users.noreply.github.com>
2021-05-05 12:39:26 -0400

    deleted react files and added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+      <footer>
+        This project is
+        <a href="https://github.com/Serendipity11/weather-react">
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
