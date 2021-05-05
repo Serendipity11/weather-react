a723242fdd242d6743b22c8706d3be777c24a05c
V S <66644986+Serendipity11@users.noreply.github.com>
2021-05-05 19:53:12 -0400

    created structure for the app

--- src/Weather.js
+++ src/Weather.js
@@ -5,11 +5,39 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3"></div>
+          <input type="submit" value="Search" />
+        </div>
+      </form>
+
       <h1>New York</h1>
       <ul>
         <li>Wednesday 08:40</li>
         <li>Mostly cloudy</li>
       </ul>
+
+      <div className="row">
+        <div className="col-6">
+          <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" />
+          6°
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 95%</li>
+            <li>Wind: 4 mph</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
