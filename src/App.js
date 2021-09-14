6a9b6d04fcf9491bc04a27815a286847f90a0004
V S <66644986+Serendipity11@users.noreply.github.com>
2021-09-14 15:27:48 -0400

    Update app.css

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ function App() {
         <div className="box">
           <Weather defaultCity="New York" />
         </div>
+        <footer>
+          This project is made by ToriS and
+          <a
+            className="github"
+            href="https://github.com/Serendipity11/weather-react"
+            target="blank"
+            rel="noopener norefferer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
       </div>
-      <footer>
-        This project is made by ToriS and
-        <a
-          className="github"
-          href="https://github.com/Serendipity11/weather-react"
-          target="blank"
-          rel="noopener norefferer"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
     </div>
   );
 }
